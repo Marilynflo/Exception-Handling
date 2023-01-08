25fff59c5f613ac30366bdf0ad78dd5b9a3783f4
mmf09 <mmf09@YPC>
2023-01-07 21:26:38 -0800

    Add project files.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try    //Exception Handling with try catch block
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex) //catching format exception 
+            {
+                //Console.WriteLine(ex.Message);
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex) //Exception to not divide by zero
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex) //General exeption
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally //Always run the program and has a database log
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
